6a902fa57e4e6ade6b2a5194330609b328ba070b
sailenog <143030517+sailenog@users.noreply.github.com>
2026-01-05 13:47:46 -0500

    LEAP first code

--- main.py
+++ main.py
@@ -8,17 +8,30 @@ output_filename = "output.json"
 if os.path.exists(output_filename):
     os.remove(output_filename)
 
+total_final = 0
 average_final = 0.0
 unique_students = 0
+students = []
 
 with open(input_filename) as f:
     reader = csv.DictReader(f)
     for row in reader:
         print(row)
+    #if unique
 
         # TODO: compute average final score
+        if row['exam_name'] == 'final':
+            total_final += float(row['score'])
+            unique_students += 1
+
+average_final = total_final/unique_students
+print(average_final)
 
         # TODO: unique student count
+        if row['student_id'] not in students:
+        students.append(row['student_id'])
+
+print(unique_students)
 
 if os.path.exists(output_filename):
     os.remove(output_filename)
